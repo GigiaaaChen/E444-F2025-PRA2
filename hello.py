ac1f5605e026c5ecc1832af3d022f6edcb102b9c
Georgia Chen <georgiachennn@gmail.com>
2025-09-20 19:10:15 -0400

    added activity 3 with navigate bar, hello name and timestamp

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
-@app.route('/user/<name>')
-def user(name):
-    return f'<h1>Hello, {name}!</h1>'
+    return render_template('index.html',
+                           name='Georgia',             
+                           current_time=datetime.utcnow())
