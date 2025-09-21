1a14e4674ce554e1a97f36bf008e414e88e4071e
Georgia Chen <georgiachennn@gmail.com>
2025-09-20 20:47:34 -0400

    added activity 1.4 with email field

--- hello.py
+++ hello.py
@@ -1,14 +1,34 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string' 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           name='Georgia',             
-                           current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data  
+        return redirect(url_for('index'))  
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name'),
+        current_time=datetime.utcnow()    
+    )
